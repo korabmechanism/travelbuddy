c07356622dbcb0d13ec309d4cd857a2400e0ac6f
unknown <brentalbarracin7@gmail.com>
2024-04-26 14:27:53 +0800

    first commit

---
+++ app/src/main/java/com/example/myappfinalproj/BottomApp.java
@@ -0,0 +1,14 @@
+package com.example.myappfinalproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_app);
+    }
+}
